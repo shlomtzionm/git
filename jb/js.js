efd732f969f061bc3b6b769af541967ee4c6098d
shlomtzion mendlovich <shlomtzosh@gmail.com>
2023-12-02 13:46:37 +0200

    hw

--- jb/js.js
+++ jb/js.js
@@ -79,3 +79,30 @@ console.log(array3)
 array = array2.concat(array3);
 console.log(array)
 
+
+
+//question 5
+ let array = [];
+ let array2 = [];
+ let array3 = [];
+
+ for (i=0; i<10; i++) { 
+   let num = +prompt("enter a number:");
+   array.push(num); 
+ }
+
+ console.log(array)
+
+for (i=0; i<array.length; i++) {
+    if (array[i] % 2 === 0) {
+        array2.push(array[i])
+    } else {
+        array3.push(array[i])
+    }
+}
+
+console.log(array2)
+
+console.log(array3)
+
+//
