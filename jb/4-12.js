7285c9a5dda531f84e5fcfbf7c3407a52ad82854
shlomtzion mendlovich <shlomtzosh@gmail.com>
2023-12-04 22:10:49 +0200

    improve

---
+++ jb/4-12.js
@@ -0,0 +1,13 @@
+// function hiUser (user, family) {
+//     console.log("hi " + user +" "+ family)
+// }
+
+// hiUser("david", "evevn")
+// hiUser("moshe", "levi")
+// hiUser("guy", "choen")
+
+function sum (num1, num2){
+console.log(num1+num2)
+}
+
+sum(2,3)
