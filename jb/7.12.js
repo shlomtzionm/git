bfa111eaad8b95aa3e14a35df8c945f287ad4998
shlomtzion <shlomtzosh@gmail.com>
2023-12-08 13:51:42 +0200

    8.12

--- jb/7.12.js
+++ jb/7.12.js
@@ -1,5 +1,21 @@
-function sumFromInput () {
-    let input = document.querySelector('.class')
-    let input2 = document.querySelector('.class2')
-    console.log(input.value+input2.value)
+// function sumFromInput () {
+//     let input = document.querySelector('.class')
+//     let input2 = document.querySelector('.class2')
+//     alert(input.value*input2.value
+
+// )
+// }
+
+// function changeTitle(){
+//     let h1input = document.querySelector('.headlineinput')
+// document.querySelector('.headline').textContent= h1input.value     
+// }
+
+
+
+function changeBackgroundColor(){
+    let input = document.querySelector('.headlinelnput')
+    let h1 = document.querySelector('.headline')
+    h1.style.backgroundColor = input.value 
+   
 }
