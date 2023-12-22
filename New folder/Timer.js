554d0ff05b9371c963bbd81c36513337ecb13de5
shlomtzion <shlomtzosh@gmail.com>
2023-12-22 16:59:10 +0200

    22.12

--- New folder/Timer.js
+++ New folder/Timer.js
@@ -1,18 +1,14 @@
+
 let input = document.querySelector('.timer');
-let btnTimer = document.querySelector('.btn-timer');
-let theTimer = document.querySelector('.theTimer');
 
 input.addEventListener('keypress', function () {
-    if (event.key === "Enter"){
+  if (event.key === "Enter") {
     let inputValue = +input.value;
-    theTimer.textContent = inputValue;
 
-   
     for (let i = inputValue; i >= 0; i--) {
-        setTimeout(() => {
-            theTimer.textContent = i;          
-        }, 1000 * (inputValue - i));
-    }}
+      setTimeout(() => {
+        input.value = i; 
+      }, 1000 * (inputValue - i));
+    }
+  }
 });
-
-document.querySelector('.container').style.animation-duration === input
