84419614b26a464834e69237d261aceff283cd68
shlomtzion mendlovich <shlomtzosh@gmail.com>
2024-01-04 17:51:08 +0200

    4.1

--- test/js.js
+++ test/js.js
@@ -2,7 +2,7 @@ function createTable (element, whichInput, tableElement){
 let element = document.createElement(tableElement)
 if (tableElement === "tr"){
     table.appendChild(element)
-} else if (tableElement === "td" && element !== pic){
+} else if (tableElement === "td" && element === tdPic){
     let imgElement = getPic(whichInput.value);
     imgElement.appendChild(imgElement)
     tr.appendChild(element)
@@ -10,3 +10,11 @@ if (tableElement === "tr"){
     element.textContent = whichInput.value
 }
 }
+
+
+createTable(row,whichInput ,tr)
+createTable(tdName, inputName, td)
+createTable(tdPrice,inputPrice,td)
+createTable(tdCategory,Category,td)
+createTable(tdPic, inputPic, td)
+
