29171aafe2b3abdb6073db2936aefd510d94ed61
shlomtzion mendlovich <shlomtzosh@gmail.com>
2024-01-28 18:58:53 +0200

    28.01

--- jb/weather.js
+++ jb/weather.js
@@ -1,6 +1,7 @@
 
 let btn = document.querySelector('.btn')
 let temp= document.querySelector('.temp')
+let card = document.querySelector(".card")
 
 btn.addEventListener('click', function(){
     getWether()
@@ -10,18 +11,18 @@ btn.addEventListener('click', function(){
 
 async function getWether(){
     let city = document.querySelector(".input").value
-    let currentWeatherUrl = `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid=${city}&appid=2950e10a28f6f2797ba9e2de2f3abcce`
+    let currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2950e10a28f6f2797ba9e2de2f3abcce&units=metric`
 
     let response = await fetch(currentWeatherUrl)
     let date = await response.json()
     console.log(date)
     printToPage(date)
+
 }
 
 function printToPage(date){
 temp.innerHTML = `${date.main.temp} &#8451;`
 }
-
 
 
 
